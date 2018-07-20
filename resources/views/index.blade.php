b8deec30522e2cefd4d5779ee51064c40ecad72f
Austin <austinsmiht@gmail.com>
2018-07-19 21:23:30 -0600

    WIP nav & secondary nav

---
+++ resources/views/index.blade.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="/css/app.css">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Freight Hub</title>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ"
+	 crossorigin="anonymous">
+</head>
+
+<body>
+	<nav class="navbar navbar-expand-lg navbar-light container-fluid">
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+		 aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNav">
+			<ul class="navbar-nav mr-auto">
+				<div class="dropdown-divider"></div>
+				<li class="nav-item phone-link">
+					<a href="tel:+4456795818" class="nav-link phone-link">
+						<i class="fas fa-phone-square"></i>+ 44 567 958 18
+					</a>
+				</li>
+				<div class="dropdown-divider"></div>
+			</ul>
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item">
+					<a class="nav-link" href="#">Blog</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Careers</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Contact Sales</a>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle language-link" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+					 aria-expanded="false">
+						<i class="fas fa-globe"></i>Language
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						<a class="dropdown-item" href="#">English</a>
+						<a class="dropdown-item" href="#">French</a>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</nav>
+
+	<div class="secondary-nav row justify-content-end">
+		<!-- <div class="col-4">
+			<ul class="list-group">
+				<li class="list-group-item">FreightHub</li>
+			</ul>
+		</div> -->
+		<div class="">
+			<ul class="list-group">
+				<li class="list-group-item">
+					<a href="#">Freight Hub</a>
+				</li class="list-group-item">
+				<li class="list-group-item">
+					<a href="#">Features</a>
+				</li class="list-group-item">
+				<li class="list-group-item">
+					<a href="#">Customer Stories</a>
+				</li class="list-group-item">
+				<li class="list-group-item">
+					<a href="#">Amazon FBA</a>
+				</li class="list-group-item">
+			</ul>
+		</div>
+		<div class="">
+			<ul class="list-group">
+				<li class="list-group-item">
+					<a href="#">About Us</a>
+				</li>
+				<li class="list-group-item">
+					<a href="#">Company</a>
+				</li>
+				<li class="list-group-item">
+					<a href="#">FAQ</a>
+				</li>
+			</ul>
+		</div>
+		<div class="">
+			<ul class="list-group">
+				<li class="list-group-item">
+					<a href="#">
+						<i class="fas fa-user-circle"></i>Sign In<i class="fas fa-angle-right"></i>
+					</a>
+				</li>
+			</ul>
+		</div>
+		<div class="">
+			<ul class="list-group"></ul>
+				<li class="list-group-item">
+					<!-- <button class="signup-button">Signup Now</button> -->
+					<a href="#" class="signup-button">Signup Now</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+
+	<!-- Script Imports-->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+	 crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+	 crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em"
+	 crossorigin="anonymous"></script>
+</body>
+
+</html>
